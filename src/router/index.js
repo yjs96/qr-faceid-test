25af51cc9f4b495c9a1933fc087fffbb6a569715
YJS96 <wnt9864@hotmail.com>
2024-09-12 14:05:27 +0900

    refresh

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MainView from '@/views/MainView.vue'
+import MainView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: MainView
     }
